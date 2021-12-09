3c5252f74649e9be5b05ce796aced33b0312f9ee
Bianca <bianca-miruna.cruceanu@student.tuiasi.ro>
2021-12-10 01:33:18 +0200

    worked on connect interface

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+import sys
 import threading
 import time
-
+import connectInterface
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import *
 import ComunicareBroker
 import client
 
-Client=client.Client()
-threading.Thread(target=ComunicareBroker.ComunicareBroker,args=(Client.socket,)).start()
-Client.socket.send(bytes.fromhex('101400044d51545405c000c000000154000155000155'))
-
-
-
+app = QtWidgets.QApplication(sys.argv)
+connect_form = connectInterface.ConnectForm()
+print(connect_form.username_value)
+connect_form.show()
+sys.exit(app.exec_())
+# Client=client.Client()
+# threading.Thread(target=ComunicareBroker.ComunicareBroker,args=(Client.socket,)).start()
+# Client.socket.send(bytes.fromhex('101400044d51545405c000c000000154000155000155'))
