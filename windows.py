e8101526fb793f39af19122d63a934228562d707
Bianca <bianca-miruna.cruceanu@student.tuiasi.ro>
2022-01-20 00:12:02 +0200

    updated main page

--- windows.py
+++ windows.py
@@ -1,3 +1,5 @@
+import time
+
 from PyQt5.QtGui import QFont, QIcon, QPainter, QPen
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget, QCheckBox, QLineEdit, \
     QTextEdit, QComboBox, QGridLayout
@@ -51,6 +53,13 @@ class PublishWindow(QWidget):
         self.QoS_box.move(75, 258)
         self.QoS_box.resize(150, 25)
 
+        # ----PUBLISH BUTTON---- #
+        self.publishButton = QPushButton("PUBLISH", self)
+        self.publishButton.move(200, 310)
+        self.publishButton.resize(100, 40)
+        self.publishButton.setStyleSheet("background-color: #ECD137;")
+        # self.publishButton.clicked.connect(self.publish_func)
+
     def set_QoS(self):
         self.QoS = self.QoS_box.currentText()  # QoS value
 
@@ -92,6 +101,13 @@ class SubscribeWindow(QWidget):
         self.QoS_box.move(80, 150)
         self.QoS_box.resize(150, 25)
 
+        # ----SUBSCRIBE BUTTON---- #
+        self.subscribeButton = QPushButton("SUBSCRIBE", self)
+        self.subscribeButton.move(200, 220)
+        self.subscribeButton.resize(100, 40)
+        self.subscribeButton.setStyleSheet("background-color: #4982F6;")
+        # self.publishButton.clicked.connect(self.subscribe_func)
+
     def set_QoS(self):
         self.QoS = self.QoS_box.currentText()  # QoS value
 
@@ -116,6 +132,33 @@ class MessagesWindow(QWidget):
         self.messages.move(190, 100)
         self.messages.resize(550, 450)
         self.messages.append('hey1')
+
+        # ----DISCONNECT BUTTON---- #
+        self.disconnectButton = QPushButton("DISCONNECT", self)
+        self.disconnectButton.move(80, 700)
+        self.disconnectButton.resize(100, 60)
+        self.disconnectButton.setStyleSheet("background-color: #F73D3A;")
+        # self.publishButton.clicked.connect(self.disconnect_func)
+
+        # ----UNSUBSCRIBE BUTTON---- #
+        self.unsubscribeButton = QPushButton("UNSUBSCRIBE", self)
+        self.unsubscribeButton.move(600, 700)
+        self.unsubscribeButton.resize(100, 40)
+        self.unsubscribeButton.setStyleSheet("background-color: #FCC5CD;")
+        # self.unsubscribeButton.clicked.connect(self.unsubscribe_func)
+
+        # ----UNSUBSCRIBE COMBOBOX--- #
+        self.unsubscribe_message = QLabel(self)
+        self.unsubscribe_message.setText("Alegeti topicul de la care vreti sa va dezabonati : ")
+        self.unsubscribe_message.move(450, 600)
+        self.unsubscribe_message.resize(350, 30)
+        self.unsubscribe_message.setFont(self.font)
+        self.unsubscribe_combobox = QComboBox(self)
+        self.unsubscribe_combobox.addItems(["0", "1", "2"])  # add items
+        self.unsubscribe_topic = self.unsubscribe_combobox.currentText()
+        # self.unsubscribe_combobox.activated.connect(self.set_unsubscribe_topic)
+        self.unsubscribe_combobox.move(550, 650)
+        self.unsubscribe_combobox.resize(200, 25)
 
 
 class MainWindow(QWidget):
@@ -155,65 +198,65 @@ class ConnectWindow(QWidget):
 
         # ----CLIENT ID---- #
         self.client_ID_check = QCheckBox(self)
-        self.client_ID_check.move(100, 150)
+        self.client_ID_check.move(100, 120)
         self.client_ID_check.resize(100, 40)
         self.client_ID_check.setText("Client ID :")
         self.client_ID_check.setStyleSheet("font-size: 10pt;")
         self.client_ID_check.stateChanged.connect(self.clientIDCheckStateChanged)
         self.client_ID = QLineEdit(self)
-        self.client_ID.move(210, 158)
+        self.client_ID.move(210, 128)
         self.client_ID.resize(200, 25)
         self.client_ID.setDisabled(True)
 
         # ---- USERNAME & PASSWORD---- #
         self.username_password_check = QCheckBox(self)
-        self.username_password_check.move(100, 230)
+        self.username_password_check.move(100, 200)
         self.username_password_check.resize(100, 40)
         self.username_password_check.stateChanged.connect(self.usernamePasswordCheckStateChanged)
 
         self.username_text = QLabel(self)
         self.username_text.setText("Username :")
-        self.username_text.move(130, 215)
+        self.username_text.move(130, 185)
         self.username_text.resize(100, 30)
         self.username_text.setStyleSheet("font-size: 10pt;")
         self.username = QLineEdit(self)
-        self.username.move(225, 220)
+        self.username.move(225, 190)
         self.username.resize(200, 25)
         self.username.setDisabled(True)
 
         self.password_text = QLabel(self)
         self.password_text.setText("Password :")
-        self.password_text.move(130, 255)
+        self.password_text.move(130, 225)
         self.password_text.resize(100, 30)
         self.password_text.setStyleSheet("font-size: 10pt;")
         self.password = QLineEdit(self)
-        self.password.move(225, 260)
+        self.password.move(225, 230)
         self.password.resize(200, 25)
         self.password.setEchoMode(QtWidgets.QLineEdit.Password)
         self.password.setDisabled(True)
 
         # ----SHOW/HIDE BUTTON---- #
         self.eye_button = QPushButton("", self)
-        self.eye_button.move(435, 260)
+        self.eye_button.move(435, 230)
         self.eye_button.resize(35, 25)
         self.eye_button.setIcon(QIcon('images/show_icon.png'))
         self.eye_button.clicked.connect(self.eye_button_func)
 
         # ----KeepAlive--- #
         self.keepAlive_check = QCheckBox(self)
-        self.keepAlive_check.move(100, 310)
+        self.keepAlive_check.move(100, 280)
         self.keepAlive_check.resize(110, 40)
         self.keepAlive_check.setText("KeepAlive :")
         self.keepAlive_check.setStyleSheet("font-size: 10pt;")
         self.keepAlive_check.stateChanged.connect(self.keepAliveCheckStateChanged)
         self.keepAlive = QLineEdit(self)
-        self.keepAlive.move(220, 320)
+        self.keepAlive.move(220, 290)
         self.keepAlive.resize(200, 25)
         self.keepAlive.setDisabled(True)
 
         # ----CleanStart---- #
         self.cleanStart_check = QCheckBox(self)
-        self.cleanStart_check.move(100, 370)
+        self.cleanStart_check.move(100, 340)
         self.cleanStart_check.resize(110, 40)
         self.cleanStart_check.setText("CleanStart :")
         self.cleanStart_check.setStyleSheet("font-size: 10pt;")
@@ -222,24 +265,34 @@ class ConnectWindow(QWidget):
         self.cleanStart_box.addItems(["0", "1"])
         self.cleanStart = self.cleanStart_box.currentText()
         self.cleanStart_box.activated.connect(self.set_cleanStart)
-        self.cleanStart_box.move(220, 380)
+        self.cleanStart_box.move(220, 350)
         self.cleanStart_box.resize(200, 25)
         self.cleanStart_box.setDisabled(True)
 
         # ----LastWill & QoS---- #
-        self.lastWill_check = QCheckBox(self)
-        self.lastWill_check.move(100, 430)
-        self.lastWill_check.resize(110, 40)
-        self.lastWill_check.setText("LastWill :")
-        self.lastWill_check.setStyleSheet("font-size: 10pt;")
-        self.lastWill_check.stateChanged.connect(self.lastWillCheckStateChanged)
-        self.lastWill = QLineEdit(self)
-        self.lastWill.move(220, 440)
-        self.lastWill.resize(200, 25)
-        self.lastWill.setDisabled(True)
+        self.last_will_topic_check = QCheckBox(self)
+        self.last_will_topic_check.move(100, 400)
+        self.last_will_topic_check.resize(130, 40)
+        self.last_will_topic_check.setText("LastWill topic:")
+        self.last_will_topic_check.setStyleSheet("font-size: 10pt;")
+        self.last_will_topic_check.stateChanged.connect(self.lastWillCheckStateChanged)
+        self.last_will_topic = QLineEdit(self)
+        self.last_will_topic.move(260, 410)
+        self.last_will_topic.resize(200, 25)
+        self.last_will_topic.setDisabled(True)
+
+        self.last_will_message_check = QLabel(self)
+        self.last_will_message_check.move(120, 430)
+        self.last_will_message_check.resize(140, 40)
+        self.last_will_message_check.setText("LastWill message:")
+        self.last_will_message_check.setStyleSheet("font-size: 10pt;")
+        self.last_will_message = QLineEdit(self)
+        self.last_will_message.move(260, 440)
+        self.last_will_message.resize(200, 25)
+        self.last_will_message.setDisabled(True)
 
         self.QoS_check = QLabel(self)
-        self.QoS_check.move(450, 430)
+        self.QoS_check.move(470, 400)
         self.QoS_check.resize(110, 40)
         self.QoS_check.setText("QoS :")
         self.QoS_check.setStyleSheet("font-size: 10pt;")
@@ -247,7 +300,7 @@ class ConnectWindow(QWidget):
         self.QoS_box.addItems(["0", "1", "2"])
         self.QoS = self.QoS_box.currentText()
         self.QoS_box.activated.connect(self.set_QoS)
-        self.QoS_box.move(510, 440)
+        self.QoS_box.move(530, 410)
         self.QoS_box.resize(200, 25)
         self.QoS_box.setDisabled(True)
 
@@ -307,16 +360,38 @@ class ConnectWindow(QWidget):
             self.cleanStart_check_enabled = False
 
     def lastWillCheckStateChanged(self):
-        if self.lastWill_check.isChecked() == True:
-            self.lastWill.setEnabled(True)
+        if self.last_will_topic_check.isChecked() == True:
+            self.last_will_topic.setEnabled(True)
+            self.last_will_message.setEnabled(True)
             self.QoS_box.setEnabled(True)
             self.lastWill_check_enabled = True
         else:
-            self.lastWill.setDisabled(True)
+            self.last_will_topic.setDisabled(True)
+            self.last_will_message.setDisabled(True)
             self.lastWill_check_enabled = False
             self.QoS_box.setDisabled(True)
 
     def connect_func(self):
         self.mainWindow = MainWindow()
         self.close()
+        # time.sleep(2)
         self.mainWindow.show()
+
+
+class SuccessWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Successful connection")
+        self.setGeometry(100, 100, 500, 300)
+
+        self.font = QtGui.QFont()
+        self.font.setBold(True)
+
+        self.text = QLabel(self)
+        self.text.setText("Conectare REUSITA !")
+        self.text.setStyleSheet("font-size: 14pt;")
+        self.text.setFont(self.font)
+        self.text.setStyleSheet("background-color: #54A227;")
+        self.text.move(50, 50)
+        self.text.resize(350, 30)
